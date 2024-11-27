f4de9f822bfb17c15b5009d631909b17d9376a8d
MarceloJSSantos <marcelojssantos2012@gmail.com>
2024-11-27 16:37:23 -0300

    Inclusão dos testes do repository

--- README.md
+++ README.md
@@ -3,10 +3,42 @@ desenvolva uma API Rest em Java"</h1>
 
 Kaban Board: <https://github.com/users/MarceloJSSantos/projects/2/views/2>
 
-<h3>Documentação das APIs (SpringDoc)</h3>
+
+<h3>DADOS BANCO DE DADOS</h3>
+No application.properties
+
+
+<h3>DOCUMENTAÇÃO DAS APIs (SpringDoc)</h3>
 
 Página da interface do usuário do Swagger
 http://localhost:8080/swagger-ui.html
 
 Descrição do OpenAPI no formato json
 http://localhost:8080/v3/api-docs
+
+<h4>Usuário para login</h4>
+
+Json da requisição para retorno do token
+```json
+{
+  "login": "ana.ferreira@business.email.com",
+  "senha": "123456"
+}
+````
+
+Json da resposta com o token
+
+```json
+{
+  "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhbmEuZmVycmVpcmFAYnVzaW5lc3MuZW1haWwuY29tIiwiaXNzIjoiQVBJIFZvbGwubWVkIiwiaWQiOjEsImV4cCI6MTczMjcyMjk4M30.4JGt1i_pKS4kkyo7Cd-2vg_LOcneFVD_Ik_RK4JfL_Q"
+}
+```
+
+Com o valor do token registrar em "Authorize"
+
+<h3>TESTES AUTOMATIZADOS ABORDADOS</h3>
+* Controlleries    ====>   APIs
+* Repositoies      ====>   Queries personalizadas
+
+Nota: Criado o BD 'vollmed_api_test' para uso dos testes
+
--- src/test/java/br/com/alura/med/voll/api/dominio/medico/MedicoRepositoryTest.java
+++ src/test/java/br/com/alura/med/voll/api/dominio/medico/MedicoRepositoryTest.java
@@ -1,18 +1,108 @@
 package br.com.alura.med.voll.api.dominio.medico;
 
+import br.com.alura.med.voll.api.dominio.consulta.Consulta;
+import br.com.alura.med.voll.api.dominio.endereco.EnderecoDados;
+import br.com.alura.med.voll.api.dominio.paciente.Paciente;
+import br.com.alura.med.voll.api.dominio.paciente.PacienteDadosCadastro;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalAdjusters;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
 class MedicoRepositoryTest {
 
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @Autowired
+    private TestEntityManager em;
+
     @Test
-    void escolherMedicoAleatorioLivreNaDataHora() {
+    @DisplayName("Deveria devolver 'null' quando único médico cadastrado não está livre na Data e Hora")
+    void escolherMedicoAleatorioLivreNaDataHoraCenario1() {
+        //given ou arrage
+        var dataHoraProximaSegundaAs10 = LocalDate.now()
+                .with(TemporalAdjusters.next(DayOfWeek.MONDAY))
+                .atTime(10, 0);
+        var especialidade = Especialidade.CARDIOLOGIA;
+
+        var medicoCadastrado  = cadastraMedico("Medico", "994555", "medico@email.com", "(21) 98888-8888", especialidade);
+        var pacienteCadastrado = cadastraPaciente("Paciente", "paciente@email.com", "(21) 97777-7777", "11122233344");
+        cadastraConsulta(medicoCadastrado, pacienteCadastrado, dataHoraProximaSegundaAs10);
+
+        //when ou act
+        var medicoLivre = medicoRepository.escolherMedicoAleatorioLivreNaDataHora(especialidade, dataHoraProximaSegundaAs10);
+
+        //then ou assert
+        assertThat(medicoLivre).isNull();
+    }
+
+    @Test
+    @DisplayName("Deveria devolver um médico quando o único médico cadastrado está livre na Data e Hora")
+    void escolherMedicoAleatorioLivreNaDataHoraCenario2() {
+        //given ou arrage
+        var dataHoraProximaSegundaAs10 = LocalDate.now()
+                .with(TemporalAdjusters.next(DayOfWeek.MONDAY))
+                .atTime(10, 0);
+        var especialidade = Especialidade.CARDIOLOGIA;
+
+        var medicoCadastrado  = cadastraMedico("Medico", "994555", "medico@email.com", "(21) 98888-8888", especialidade);
+
+        //when ou act
+        var medicoLivre = medicoRepository.escolherMedicoAleatorioLivreNaDataHora(especialidade, dataHoraProximaSegundaAs10);
+
+        //then ou assert
+        assertThat(medicoLivre).isEqualTo(medicoCadastrado);
+    }
+
+
+    //métodos auxiliares
+    private void cadastraConsulta(Medico medico, Paciente paciente, LocalDateTime dataHora){
+        em.persist(new Consulta(null, paciente, medico, dataHora, null));
+    }
+
+    private Medico cadastraMedico(String nome, String crm, String email, String telefone, Especialidade especialidade){
+
+        var medico = new Medico(medicoDadosCadastro(nome, crm, email, telefone, especialidade));
+        em.persist(medico);
+        return medico;
+    }
+
+    private MedicoDadosCadastro medicoDadosCadastro(String nome, String crm, String email, String telefone, Especialidade especialidade){
+        return new MedicoDadosCadastro(nome, crm, email, telefone, especialidade, enderecoDados());
+    }
+
+    private Paciente cadastraPaciente(String nome, String email, String telefone, String cpf){
+        var paciente = new Paciente(pacienteDadosCadastro(nome, email, telefone,cpf));
+        em.persist(paciente);
+        return paciente;
+    }
+
+    private PacienteDadosCadastro pacienteDadosCadastro(String nome, String email, String telefone, String cpf){
+        return new PacienteDadosCadastro(nome, email, telefone, cpf, enderecoDados());
+    }
+
+    private EnderecoDados enderecoDados(){
+        return  new EnderecoDados(
+                "rua a",
+                "bairro",
+                "11000-000",
+                "cidade",
+                "RJ",
+                "100",
+                "compl. A");
     }
 }
